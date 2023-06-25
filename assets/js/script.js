0c6dfa6628961b90f2ee0bc18838a775f47b50d1
John Helder <johnhelderjp@gmail.com>
2023-06-25 10:03:01 -0300

    Criando a função que abre e fecha o menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@ let show = true;
 const menuContent = document.querySelector('.content');
 const menuToglle = menuContent.querySelector('.menu-toggle');
 
+menuToglle.addEventListener("click", ()=>{
+  menuContent.classList.toggle('on', show);
+  show = !show;
+});
